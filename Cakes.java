1d37f42e666aa8047588bb55945d59fa2a9e52d4
AndreaYuanSerrano <andreayuanserrano@gmail.com>
2025-01-14 15:54:59 +0800

    Update Cakes.java

--- Cakes.java
+++ Cakes.java
@@ -5,7 +5,9 @@ public class Cakes
 	
     
    System.out.println(SweetestCake.CARROT.getRatingsOfCakes());
-	  
+	   System.out.println(SweetestCake.BLUEBERRY.getRatingsOfCakes());
+   System.out.println(SweetestCake.STRAWBERRY.getRatingsOfCakes());
+   System.out.println(SweetestCake.CHOCOLATE.getRatingsOfCakes());  
    }
 
 
